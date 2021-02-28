0f8a2cc629b12d0e7cfedcb110f1961ff49b145a
Techdoodle <chris@techchrism.me>
2021-02-27 21:50:18 -0800

    solve synchronizing lists

---
+++ solutions/synchronizing-lists.cpp
@@ -0,0 +1,84 @@
+// Problem https://open.kattis.com/problems/synchronizinglists
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <ctime>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cassert>
+
+using namespace std;
+
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int, int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int ul;
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    while(true)
+    {
+        int n;
+        cin>>n;
+        if(n==0)
+            break;
+        PII f[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>f[i].first;
+            f[i].second = i;
+        }
+        sort(f,f+n);
+        int s[n];
+        int final[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>s[i];
+        }
+        sort(s,s+n);
+        for(int i=0;i<n;i++)
+        {
+            final[f[i].second]=s[i];
+        }
+        for(int x:final)
+            cout<<x<<"\n";
+        cout<<"\n";
+    }
+
+    return 0;
+}
