6b0b1712eff872e7f465ece8c9861a8a8d8e2186
Techdoodle <chris@techchrism.me>
2021-02-27 16:25:41 -0800

    solve detailed differences

---
+++ solutions/detailed-differences.cpp
@@ -0,0 +1,69 @@
+// Problem https://open.kattis.com/problems/detaileddifferences
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <ctime>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cassert>
+
+using namespace std;
+
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int, int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int ul;
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        string s1,s2;
+        cin>>s1>>s2;
+        cout<<s1<<"\n"<<s2<<"\n";
+        for(int j=0;j<s1.length();j++)
+        {
+            cout<<((s1[j]==s2[j])?".":"*");
+        }
+        cout<<"\n\n";
+    }
+
+    return 0;
+}
