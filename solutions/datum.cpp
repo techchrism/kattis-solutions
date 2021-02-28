af0835056ebc52a08ff435a7aa621b6011017f13
Techdoodle <chris@techchrism.me>
2021-02-27 16:35:04 -0800

    solve datum

---
+++ solutions/datum.cpp
@@ -0,0 +1,69 @@
+// Problem https://open.kattis.com/problems/datum
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <ctime>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cassert>
+
+using namespace std;
+
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int, int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int ul;
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int dperm[]={31,28,31,30,31,30,31,31,30,31,30,31};
+    string wdays[]={"Wednesday","Thursday","Friday","Saturday","Sunday","Monday","Tuesday"};
+
+    int d,m;
+    cin>>d>>m;
+    m--;
+    int t = 0;
+    for(int i=0;i<m;i++)
+    {
+        t += dperm[i];
+    }
+    t += d;
+    cout<<wdays[(t%7)];
+
+    return 0;
+}
