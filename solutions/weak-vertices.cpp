443247860b5060e8f5a4c56947bed3629d5c7c61
techchrism <chris@techchrism.me>
2021-03-08 21:39:11 -0800

    solve weak vertices

---
+++ solutions/weak-vertices.cpp
@@ -0,0 +1,88 @@
+// Problem https://open.kattis.com/problems/weakvertices
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <ctime>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cassert>
+
+using namespace std;
+
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int, int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int ul;
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    while(true)
+    {
+        int n;
+        cin>>n;
+        if(n==-1)
+            break;
+        bool m[n][n];
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                cin>>m[i][j];
+            }
+        }
+        for(int i=0;i<n;i++) {
+            bool f=false;
+            for(int j=0;j<n&&!f;j++) {
+                if(m[i][j]) {
+                    for(int k=0;k<n&&!f;k++) {
+                        if(m[j][k]) {
+                            for(int l = 0; l < n&&!f; l++) {
+                                if(m[i][k]) {
+                                    f=true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if(!f)
+                cout<<i<<" ";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
