c8c200ea069073199666644fb5b7f24095834508
techchrism <chris@techchrism.me>
2021-03-09 13:40:58 -0800

    solve star arrangements

---
+++ solutions/star-arrangements.cpp
@@ -0,0 +1,69 @@
+// Problem https://open.kattis.com/problems/stararrangements
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <ctime>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cassert>
+
+using namespace std;
+
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int, int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int ul;
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int s;
+    cin>>s;
+    cout<<s<<":\n";
+    for(int i=2;i<(s/2)+2;i++)
+    {
+        for(int j=1;j>=0;j--)
+        {
+            if(s%(i+i-j)==0||(s-i)%(i+i-j)==0)
+            {
+                cout<<i<<","<<i-j<<"\n";
+            }
+        }
+    }
+
+    return 0;
+}
