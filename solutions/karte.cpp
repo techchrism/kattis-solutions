7b7cf67f8cbf5ac438fae36531f8d2fece41b0d2
techchrism <chris@techchrism.me>
2021-05-25 08:52:01 -0700

    solve karte

---
+++ solutions/karte.cpp
@@ -0,0 +1,77 @@
+// Problem https://open.kattis.com/problems/karte
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <ctime>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cassert>
+
+using namespace std;
+
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int, int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int ul;
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    bool c[13][13];
+    memset(c,false,13*13);
+    string s;
+    cin>>s;
+    for(int i=0;i<s.length();i+=3){
+        int su=s[i]-'H';
+        int v=stoi(s.substr(i+1, 2))-1;
+        if(c[su][v]){
+            cout<<"GRESKA";
+            return 0;
+        }
+        c[su][v]=true;
+    }
+    for(char su:"PKHT"){
+        if(su=='\0') continue;
+        int f=0;
+        for(int i=0;i<13;i++){
+            if(!c[su-'H'][i]) f++;
+        }
+        cout<<f<<" ";
+    }
+
+    return 0;
+}
