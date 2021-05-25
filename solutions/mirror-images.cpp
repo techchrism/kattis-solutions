c727278f237eedd2e611a601abb29396e1155028
techchrism <chris@techchrism.me>
2021-05-24 21:36:23 -0700

    solve mirror images

---
+++ solutions/mirror-images.cpp
@@ -0,0 +1,71 @@
+// Problem https://open.kattis.com/problems/mirror
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <ctime>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cassert>
+
+using namespace std;
+
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int, int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int ul;
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<"Test "<<(i+1)<<"\n";
+        int r,c;
+        cin>>r>>c;
+        string s[r];
+        for(string &si:s){
+            cin>>si;
+            reverse(si.begin(), si.end());
+        }
+        for(int y=r-1;y>=0;y--){
+            cout<<s[y]<<"\n";
+        }
+    }
+
+    return 0;
+}
