5e652a3b258f14e56581d94705d9c6cf094d7699
techchrism <chris@techchrism.me>
2021-09-03 13:53:04 -0700

    solve different distances

---
+++ solutions/different-distances.cpp
@@ -0,0 +1,63 @@
+// Problem https://open.kattis.com/problems/differentdistances
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <ctime>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cassert>
+
+using namespace std;
+
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int, int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int ul;
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    while(true){
+        double x1,y1,x2,y2,p;
+        cin>>x1;
+        if(x1==0) break;
+        cin>>y1>>x2>>y2>>p;
+        cout<<fixed<<pow(pow(abs(x1-x2),p)+pow(abs(y1-y2),p),(1.0/p))<<"\n";
+    }
+
+    return 0;
+}
