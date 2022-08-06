25efd66e481a9080c370b6ea6b5678237d75bd99
techchrism <chris@techchrism.me>
2022-08-06 16:11:02 -0700

    solve a rational sequence 3

---
+++ solutions/a-rational-sequence-3.cpp
@@ -0,0 +1,80 @@
+// Problem https://open.kattis.com/problems/rationalsequence3
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <ctime>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cassert>
+
+using namespace std;
+
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int, int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int ul;
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int32 p,n,k;
+    cin>>p;
+    for(int i=0;i<p;i++){
+        cin>>k>>n;
+        vector<char> c;
+        while(n>1){
+            if(n%2==1){
+                n--;
+                c.PB('r');
+            }else{
+                c.PB('l');
+            }
+            n/=2;
+        }
+        reverse(c.begin(), c.end());
+        int32 pn=1,q=1;
+        for(char ch:c){
+            if(ch=='r'){
+                pn+=q;
+            }else{
+                q+=pn;
+            }
+        }
+        cout<<k<<" "<<pn<<"/"<<q<<"\n";
+    }
+
+    return 0;
+}
