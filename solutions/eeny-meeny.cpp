a860f835a7ab6816b9617ef91b41db37d103703d
techchrism <chris@techchrism.me>
2022-08-29 00:18:35 -0700

    solve eeny meeny

---
+++ solutions/eeny-meeny.cpp
@@ -0,0 +1,82 @@
+// Problem https://open.kattis.com/problems/eenymeeny
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <ctime>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cassert>
+
+using namespace std;
+
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int, int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int ul;
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    string s;
+    getline(cin,s);
+
+    int n;
+    cin>>n;
+    VS l;
+    for(int i=0;i<n;i++){
+        string ln;
+        cin>>ln;
+        l.PB(ln);
+    }
+
+    int sk=count(s.begin(),s.end(),' ');
+    VS a,b;
+    bool al=false;
+    int p=0;
+    while(!l.empty()){
+        p=(p+sk)%l.size();
+        (al?b:a).PB(l[p]);
+        al=!al;
+        l.erase(l.begin()+p);
+    }
+
+    cout<<a.size()<<"\n";
+    for(auto i:a)cout<<i<<"\n";
+    cout<<b.size()<<"\n";
+    for(auto i:b)cout<<i<<"\n";
+
+    return 0;
+}
